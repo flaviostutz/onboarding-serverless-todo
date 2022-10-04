808c3c717b6159857d564947d862668ea2c7fc7d
Niels Mijnster <niels.mijnster@nn-group.com>
2022-10-04 13:37:49 +0200

    2

--- src/cow-slug.ts
+++ src/cow-slug.ts
@@ -1,4 +1,4 @@
 import think from 'cowsay';
 import { generateSlug } from 'random-word-slugs';
 
-console.log(think.say({ text: generateSlug() }));
+  console.log(think.say({ text: generateSlug() }));
