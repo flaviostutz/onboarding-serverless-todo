e1dea79dbbe36a511d524feefe5f8eb81895c60d
Niels Mijnster <niels.mijnster@nn-group.com>
2022-10-04 09:56:46 +0200

    21

--- jest.config.js
+++ jest.config.js
@@ -3,14 +3,21 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/?(*.)+(spec|test).+(ts|tsx|js)'],
-  "transform": {
-    "^.+\\.jsx?$": [
-      "esbuild-jest-transform",
-      {
-        "target": "ESNEXT",
-        sourcemap: true, // correct line numbers in code coverage
-
-      },
-    ],
-  },
+  transform: {
+      '\\.^.+\\.jsx?$': 'esbuild-jest-transform',
+    },
 };
+
+
+
+
+// "transform": {
+//   "^.+\\.jsx?$": [
+//     "esbuild-jest-transform",
+//     {
+//       "target": "ESNEXT",
+//       sourcemap: true, // correct line numbers in code coverage
+
+//     },
+//   ],
+// },
